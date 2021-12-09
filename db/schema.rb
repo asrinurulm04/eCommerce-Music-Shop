c85b5b403bce24f147ffdc0d36b44792fff1fbaa
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-09 21:47:35 +0700

    tampilan new instruments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_140748) do
+ActiveRecord::Schema.define(version: 2021_12_09_144423) do
+
+  create_table "instruments", charset: "utf8mb4", force: :cascade do |t|
+    t.string "brand"
+    t.string "model"
+    t.text "description"
+    t.string "condition"
+    t.string "finish"
+    t.string "title"
+    t.decimal "price", precision: 5, scale: 2, default: "0.0"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.integer "user_id"
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
