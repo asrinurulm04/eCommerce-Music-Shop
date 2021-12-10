0e4afdf9f158dca4e0ff8fe7caed6a2a9a053147
Asri Nurul Ma'rifah <asrimarifah0402@gmail.com>
2021-12-10 21:31:27 +0700

    add carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :carts
   resources :instruments
   devise_for :users, controllers: {
     registrations: 'registrations'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_144423) do
+ActiveRecord::Schema.define(version: 2021_12_10_142929) do
+
+  create_table "carts", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "instruments", charset: "utf8mb4", force: :cascade do |t|
     t.string "brand"
